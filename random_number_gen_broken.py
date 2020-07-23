2be682684463906e8e92a53ad74b7b256adb4dca
John Sun <john.191101@outlook.com>
2020-07-22 17:15:55 -0700

    fixed rng

--- random_number_gen_broken.py
+++ random_number_gen_broken.py
@@ -9,14 +9,14 @@ def rng(seed, a, c, modulo, iter):
 
 rng(1000,24693,3967,2**15,3)
 
-"""
-(from wikipedia) 
-"""
+# """
+# (from wikipedia) 
+# """
 
 
-def lcg(modulus, a, c, seed):
-    """Linear congruential generator."""
-    for i in range(3):
-        seed = (a * seed + c) % modulus
-        yield seed
+# def lcg(modulus, a, c, seed):
+#     """Linear congruential generator."""
+#     for i in range(3):
+#         seed = (a * seed + c) % modulus
+#         yield seed
 
